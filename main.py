5f24ba8a23768173764ed035298d078b085342e1
Alexsei Popov <popov2895@gmail.com>
2023-08-14 17:31:17 +0300

    create Ingredient table

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlalchemy as sa
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,6 +10,12 @@ db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
+
+
+class Ingredient(db.Model):
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.Text, nullable=False, unique=True)
+
 
 if __name__ == '__main__':
     app.run(port=5005, debug=True, host='0.0.0.0')

d4258c8cccfb504681c5ecd41e4940ed85c90ec9
Dariazhuravleva <darya.spring@mail.ru>
2023-08-21 14:52:36 +0300

    migrate db ingredient

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sqlalchemy as sa
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
@@ -15,6 +17,10 @@ with app.app_context():
 class Ingredient(db.Model):
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.Text, nullable=False, unique=True)
+    protein = sa.Column(sa.Integer)
+    fat = sa.Column(sa.Integer)
+    carb = sa.Column(sa.Integer)
+    calories = sa.Column(sa.Integer)
 
 
 if __name__ == '__main__':
